24ba67c316437f823fd262e482fc2da154cc590b
Robb Enzmann <robbenzmann@gmail.com>
2022-01-20 09:16:15 -0500

    put branch back in

--- .func.sh
+++ .func.sh
@@ -31,7 +31,7 @@ vim-add () {
   local plugin="$2"
   if [ -z $branch ]; then local branch="master"; fi
   cd $HOME
-  git subtree add -P .vim/pack/$author/start/$plugin git@github.com:$author/$plugin --squash
+  git subtree add -P .vim/pack/$author/start/$plugin git@github.com:$author/$plugin $branch --squash
   cd -
 }
 
