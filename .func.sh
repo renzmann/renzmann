a8f206557218dd3109a3faade485f731fc28ca41
Robb Enzmann <robbenzmann@gmail.com>
2022-01-19 22:30:31 -0500

    add a skeleton for the vim-add

--- .func.sh
+++ .func.sh
@@ -10,13 +10,6 @@ ll () { ls -lah; }
 la () { ls -A; }
 l  () { ls -CF; }
 
-this-fails () {
-  echo $1
-  local name=$1
-  linenum=$(grep -En "^$name \(" $HOME/.func.sh | awk -F ':' '{ print $1 }')
-  echo $linenum
-}
-
 so () { source .venv/bin/activate; }
 
 new-venv () {
@@ -26,3 +19,8 @@ new-venv () {
   python3 -m pip install wheel
 }
 
+vim-add () {
+  # Should get something like tpope/vim-sensible and then execute:
+  # git subtree add -P .vim/pack/tpope/start/vim-sensible git@github.com:tpope/vim-sensible --squash
+  ...
+}
