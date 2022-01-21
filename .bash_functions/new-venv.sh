cc3fa18165e74addc7400e62ee6a4fff5874a634
Robb Enzmann <robbenzmann@gmail.com>
2022-01-20 22:18:38 -0500

    add option to new-venv for name

--- .bash_functions/new-venv.sh
+++ .bash_functions/new-venv.sh
@@ -1,7 +1,12 @@
 new-venv () {
-  python3 -m venv .venv
-  so
-  python3 -m pip install --upgrade pip
-  python3 -m pip install wheel
+  if [ -z "$1" ]; then
+    local name=".venv"
+  else
+    local name="$1"
+  fi
+
+  python3 -m venv $name \
+    && source $name/bin/activate \
+    && python3 -m pip install --upgrade pip wheel
 }
 
