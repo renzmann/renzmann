2452b16504f76efba24d9aeffdf0d05087cc8e0d
Robb Enzmann <robbenzmann@gmail.com>
2022-01-25 20:04:19 -0500

    remove jabba and mill install scripts

--- .bash_functions/install-jabba.sh
+++
@@ -1,6 +0,0 @@
-install-jabba() {
-  echo "JABBA_VERSION=$JABBA_VERSION"
-  curl -sL https://github.com/shyiko/jabba/raw/master/install.sh \
-    | bash -s -- --skip-rc \
-    && . ~/.jabba/jabba.sh
-}
