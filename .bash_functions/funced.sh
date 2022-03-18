d07ec3be74666b9d671db03c84edd3756f1242d7
Robb Enzmann <robbenzmann@gmail.com>
2022-03-18 10:57:19 -0400

    enhancements to funced

--- .bash_functions/funced.sh
+++ .bash_functions/funced.sh
@@ -1,17 +1,27 @@
+#!/usr/bin/env bash
 funced() {
-  if command -v nvim &> /dev/null; then
-    local editor=nvim
-  else
-    local editor=vim
-  fi
+	if command -v nvim &> /dev/null; then
+		local editor=nvim
+	else
+		local editor=vim
+	fi
 
-  $editor $HOME/.bash_functions/$(basename $1).sh && load-funcs
+	local funcname=$(basename $1)
+	local fp=$HOME/.bash_functions/${funcname}.sh
+	local template="#!/usr/bin/env bash\n${funcname}()\n{	\n}\n"
+	[[ ! -f $fp ]] && cat > $fp <<- EOF
+		#!/usr/bin/env bash
+		${funcname}() {
+			echo hello
+		}
+		EOF
+	$editor +3 $fp && load-funcs
 }
 
 _funced() {
-    local cur=${COMP_WORDS[COMP_CWORD]}
-    local funcs=$(declare -F | awk '{ print $3 }' | tr '\n' ' ')
-    COMPREPLY=( $(compgen -W "$funcs" -- $cur) )
+	local cur=${COMP_WORDS[COMP_CWORD]}
+	local funcs=$(declare -F | awk '{ print $3 }' | tr '\n' ' ')
+	COMPREPLY=( $(compgen -W "$funcs" -- $cur) )
 }
 
 complete -F _funced funced
