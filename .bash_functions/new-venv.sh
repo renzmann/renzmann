03be719e7fe454356206d687a6d5542ec42db123
Robb Enzmann <robbenzmann@gmail.com>
2022-03-31 08:10:33 -0400

    add python version variable to new-venv

--- .bash_functions/new-venv.sh
+++ .bash_functions/new-venv.sh
@@ -1,12 +1,8 @@
 new-venv() {
-  if [ -z "$1" ]; then
-    local name=".venv"
-  else
-    local name="$1"
-  fi
+	local name=${1:-.venv}
 
-  python3 -m venv $name \
-    && source $name/bin/activate \
-    && python3 -m pip install --upgrade pip wheel
+	python3 -m venv $name \
+		&& source $name/bin/activate \
+		&& python3 -m pip install --upgrade pip wheel
 }
 
