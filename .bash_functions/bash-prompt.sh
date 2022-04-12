d5dbb4cd0065f4c3124d0d19b86b2810b3f13d58
Robb Enzmann <robbenzmann@gmail.com>
2022-04-12 13:59:48 -0400

    break PS1 into parts

--- .bash_functions/bash-prompt.sh
+++ .bash_functions/bash-prompt.sh
@@ -1,36 +1,38 @@
 #!/usr/bin/env bash
 bash-prompt() {
 	# set a fancy prompt (non-color, unless we know we "want" color)
+	local deb_part='${debian_chroot:+($debian_chroot)}'
+	local user_part=${LIGHT_BLUE}'\u'${COLOR_RESET}
+	local user_sep=${LIGHT_GRAY}@${COLOR_RESET}
+	local host_part=${YELLOW}'\h'${COLOR_RESET}
+	local dir_sep=${LIGHT_GRAY}:${COLOR_RESET}
+	local dir_part=${CYAN}'$(abbrev-pwd)'${COLOR_RESET}
+	local git_part=${LIGHT_RED}'$(__git_ps1)'${COLOR_RESET}
+	local prompt_part='\n'${GREEN}'>'${COLOR_RESET}' '
+	# local force_color_prompt=yes
+
 	case "$TERM" in
-		screen|*-color|*-256color|dumb) color_prompt=yes;;
+		screen|*-color|*-256color|dumb)
+			local color_prompt=yes
+			;;
+		*)
+			;;
 	esac
 
-	# force_color_prompt=yes
 	if [[ -n "$force_color_prompt" ]]; then
 		if [[ -x /usr/bin/tput ]] && tput setaf 1 >&/dev/null; then
 			# We have color support; assume it's compliant with Ecma-48
 			# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
 			# a case would tend to support setf rather than setaf.)
-			color_prompt=yes
+			local color_prompt=yes
 		else
-		color_prompt=
+			local color_prompt=
 		fi
 	fi
 
 	if [[ "$color_prompt" = yes ]]; then
-		PS1="${debian_chroot:+($debian_chroot)}${LIGHT_BLUE}\u${LIGHT_GRAY}@${YELLOW}\h${LIGHT_GRAY}:${CYAN}$(abbrev-pwd)${LIGHT_GRAY}$(__git_ps1)${COLOR_RESET}\n${GREEN}>${COLOR_RESET} "
+		PS1=${deb_part}${user_part}${user_sep}${host_part}${dir_sep}${dir_part}${git_part}${prompt_part}
 	else
-		PS1="${debian_chroot:+($debian_chroot)}\u@\h:\w$(__git_ps1)\n\$ "
+		PS1=${deb_part}'\u@\h:\w$(__git_ps1)\n\$ '
 	fi
-
-	unset color_prompt force_color_prompt
-
-	# If this is an xterm set the title to user@host:dir
-	case "$TERM" in
-		xterm*|rxvt*)
-			PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-			;;
-		*)
-			;;
-	esac
 }
