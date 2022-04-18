d3710309ba82e9bee76b6d48923cfb3a035bed53
Robb Enzmann <robbenzmann@gmail.com>
2022-04-18 08:49:48 -0400

    merge "so" and "new-venv"

--- .bash_functions/new-venv.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-new-venv() {
-	local name=${1:-.venv}
-	local version=${2:-3}
-
-	python${version} -m venv $name \
-		&& source $name/bin/activate \
-		&& python${version} -m pip install --upgrade pip wheel
-}
--- .bash_functions/so.sh
+++ .bash_functions/so.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 so() {
 	local name=${1:-.venv}
+	local version=${2:-3}
+
 	if [[ ! -d $name ]]; then
-		new-venv $name
+		python${version} -m venv $name \
+			&& source $name/bin/activate \
+			&& python${version} -m pip install --upgrade pip wheel
 	else
 		source $name/bin/activate
 	fi
