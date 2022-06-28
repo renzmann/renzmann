9035068530bed6f8255fa2b819bb0f416a8f03fd
renzmann <32076780+renzmann@users.noreply.github.com>
2022-06-27 23:39:54 -0400

    add lsp configuration to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,152 @@
 if vim.fn.has('win32')
 then
-	vim.cmd('source ~/vimfiles/vimrc')
+   vim.cmd('source ~/vimfiles/vimrc')
 else
-	vim.cmd('source ~/.vim/vimrc')
+   vim.cmd('source ~/.vim/vimrc')
 end
+
+-- Setup nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+   snippet = {
+   -- REQUIRED - you must specify a snippet engine
+   expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+   end,
+   },
+   window = {
+   -- completion = cmp.config.window.bordered(),
+   -- documentation = cmp.config.window.bordered(),
+   },
+   mapping = cmp.mapping.preset.insert({
+   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+   ['<C-f>'] = cmp.mapping.scroll_docs(4),
+   ['<C-Space>'] = cmp.mapping.complete(),
+   ['<C-e>'] = cmp.mapping.abort(),
+   ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+   -- Wonky ass error when doing TAB inside python function
+   -- ["<Tab>"] = cmp.mapping(function(fallback)
+   --   if cmp.visible() then
+   --     cmp.select_next_item()
+   --   elseif vim.fn["vsnip#available"](1) == 1 then
+   --     feedkey("<Plug>(vsnip-expand-or-jump)", "")
+   --   elseif has_words_before() then
+   --     cmp.complete()
+   --   else
+   --     fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+   --   end
+   -- end, { "i", "s" }),
+
+   -- ["<S-Tab>"] = cmp.mapping(function()
+   --   if cmp.visible() then
+   --     cmp.select_prev_item()
+   --   elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+   --     feedkey("<Plug>(vsnip-jump-prev)", "")
+   --   end
+   -- end, { "i", "s" }),
+   }),
+   sources = cmp.config.sources({
+   { name = 'nvim_lsp' },
+   { name = 'vsnip' }, -- For vsnip users.
+   -- { name = 'luasnip' }, -- For luasnip users.
+   -- { name = 'ultisnips' }, -- For ultisnips users.
+   -- { name = 'snippy' }, -- For snippy users.
+   }, {
+   { name = 'buffer' },
+   })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+   sources = cmp.config.sources({
+   { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+   }, {
+   { name = 'buffer' },
+   })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+   mapping = cmp.mapping.preset.cmdline(),
+   sources = {
+   { name = 'buffer' }
+   }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline(':', {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = cmp.config.sources({
+--     { name = 'path' }
+--   }, {
+--     { name = 'cmdline' }
+--   })
+-- })
+
+require('nvim-lsp-installer').setup {}
+local lspconfig = require('lspconfig')
+
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local opts = { noremap=true, silent=true }
+
+  -- The bindings here override very similar ones in base vim, and isn't a spot
+  -- to add new bindings.  See the ./after/plugin/keymap.vim for those.
+  buf_set_keymap('n', 'gD', '<CMD>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<CMD>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K',  '<CMD>lua vim.lsp.buf.hover()<CR>', opts)
+end
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local servers = {
+  "bashls",
+  "yamlls",
+  "rust_analyzer",
+  "pyright",
+}
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup { on_attach = on_attach, capabilities = capabilities }
+end
+
+-- gopls setup
+-- Make sure that $GOPATH/bin is on $PATH after installing gopls for this to work
+-- cd $HOME && mkdir -p tmp && cd tmp && go install golang.org/x/tools/gopls@latest
+lspconfig["gopls"].setup { cmd = {'gopls', '--remote=auto'}, capabilities = capabilities }
+
+-- julia setup
+-- Make sure to run julia and from the package manager prompt:
+-- ] add LanguageServer SymbolServer
+lspconfig["julials"].setup {
+  cmd = {
+    "julia",
+    "--startup-file=no",
+    "--history-file=no",
+    "-e", [[
+      using Pkg;
+      Pkg.instantiate()
+      using LanguageServer; using SymbolServer;
+      depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
+      project_path = dirname(something(Base.current_project(pwd()), Base.load_path_expand(LOAD_PATH[2])))
+      # Make sure that we only load packages from this environment specifically.
+      @info "Running language server" env=Base.load_path()[1] pwd() project_path depot_path
+      server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path);
+      server.runlinter = true;
+      run(server);
+    ]]
+  },
+  capabilities = capabilities,
+}
+
+-- Uncomment to disable diagnostics
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+
+-- Uncomment to disable location list of diagnostics
+-- lua vim.lsp.diagnostic.set_loclist({open_loclist = false})
+
