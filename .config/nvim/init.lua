227c3e81112672e4396d133cde0835567caf1d7d
Robb Enzmann <robbenzmann@gmail.com>
2022-07-06 10:26:11 -0400

    add cmp back into command line

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -129,19 +129,19 @@ cmp.setup.filetype('gitcommit', {
 cmp.setup.cmdline('/', {
    mapping = cmp.mapping.preset.cmdline(),
    sources = {
-   { name = 'buffer' }
+      { name = 'buffer' }
    }
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline(':', {
---   mapping = cmp.mapping.preset.cmdline(),
---   sources = cmp.config.sources({
---     { name = 'path' }
---   }, {
---     { name = 'cmdline' }
---   })
--- })
+cmp.setup.cmdline(':', {
+   mapping = cmp.mapping.preset.cmdline(),
+   sources = cmp.config.sources({
+      { name = 'path' }
+   }, {
+      { name = 'cmdline' }
+   })
+})
 -- }}}
 
 -- LSP (Language Servers) {{{
