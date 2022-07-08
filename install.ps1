42a2814982202b57315399526a732a6446d9d9ed
renzmann <32076780+renzmann@users.noreply.github.com>
2022-07-08 12:03:45 -0400

    add terminal settings to install script

--- install.ps1
+++ install.ps1
@@ -1,14 +1,24 @@
+# Neovim {{{
 if (Test-Path $Env:XDG_CONFIG_HOME\nvim) {
 	Remove-Item -Force -Recurse $Env:XDG_CONFIG_HOME\nvim
 }
 New-Item -Path $Env:XDG_CONFIG_HOME\nvim -ItemType SymbolicLink -Value $PSScriptRoot\.config\nvim
+# }}}
 
+# vimrc {{{
 if (Test-Path $HOME\vimfiles) {
 	Remove-Item -Force -Recurse $HOME\vimfiles
 }
 New-Item -Path $HOME\vimfiles -ItemType SymbolicLink -Value $PSScriptRoot\.vim
+# }}}
 
+# Powershell profile {{{
 if (Test-Path $profile) {
 	Remove-Item -Force $profile
 }
 New-Item -Path $profile -ItemType SymbolicLink -Value $PSScriptRoot\Microsoft.PowerShell_profile.ps1
+# }}}
+
+# Windows terminal settings {{{
+# Copy-Item -Path $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -ItemType SymbolicLink -Value $PSScriptRoot\windows_terminal_settings.json
+# }}}
