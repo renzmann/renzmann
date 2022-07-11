1439ee1a5f5d44d6629d5c0c7ec87fa6c073c92e
renzmann <32076780+renzmann@users.noreply.github.com>
2022-07-11 19:03:28 -0400

    nginx.vim --> nginx.lua

---
+++ .config/nvim/after/ftplugin/nginx.lua
@@ -0,0 +1,7 @@
+local o = vim.opt_local
+o.expandtab = false
+o.tabstop = 8
+o.softtabstop = 8
+o.shiftwidth = 8
+o.textwidth = 120
+o.fileformat = unix
--- .config/nvim/after/ftplugin/nginx.vim
+++
@@ -1,5 +0,0 @@
-setlocal noexpandtab
-setlocal tabstop=8
-setlocal softtabstop=8
-setlocal shiftwidth=8
-setlocal textwidth=120
