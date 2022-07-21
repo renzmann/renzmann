34c6d3e6d8055f0e95f1812eb3b4c08c7bb90793
Robb Enzmann <robbenzmann@gmail.com>
2022-07-21 19:48:40 +0000

    remove old tab-complete style stuff from cmp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -188,28 +188,7 @@ cmp.setup {
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-
-      -- Wonky ass error when doing TAB inside python function
-      -- ["<Tab>"] = cmp.mapping(function(fallback)
-      --   if cmp.visible() then
-      --     cmp.select_next_item()
-      --   elseif vim.fn["vsnip#available"](1) == 1 then
-      --     feedkey("<Plug>(vsnip-expand-or-jump)", "")
-      --   elseif has_words_before() then
-      --     cmp.complete()
-      --   else
-      --     fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-      --   end
-      -- end, { "i", "s" }),
-
-      -- ["<S-Tab>"] = cmp.mapping(function()
-      --   if cmp.visible() then
-      --     cmp.select_prev_item()
-      --   elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-      --     feedkey("<Plug>(vsnip-jump-prev)", "")
-      --   end
-      -- end, { "i", "s" }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
    },
 
    sources = cmp.config.sources({
