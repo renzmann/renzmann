65b804fa0d26529d8047c7e5c916e661c0426bb1
Robb Enzmann <robbenzmann@gmail.com>
2022-09-30 12:35:42 -0400

    add blacken, org-roam, sql-indent to package list

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -48,3 +48,7 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+
+(package! blacken)
+(package! org-roam)
+(package! sql-indent)
